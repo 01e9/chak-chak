77ecddcddc95762ae59bf448b71a3cf6d7d1de29
arteniioleg <oleg@artenii.email>
2018-08-18 20:15:19 +0300

    created `selectToolbarPosition()`

---
+++ src/redux/selectors/toolbar.js
@@ -0,0 +1,4 @@
+export const selectToolbarPosition = (state) => ({
+    x: state.toolbar.x,
+    y: state.toolbar.y
+})
---
+++ src/redux/selectors/toolbar.test.js
@@ -0,0 +1,10 @@
+import createReduceRoot from "@/redux/reducers";
+import ImageStorage from "@/storage/ImageStorage";
+import { actionToolbarPositionSet } from "@/redux/actions/toolbar";
+import { selectToolbarPosition } from "@/redux/selectors/toolbar";
+
+it('works', () => {
+    const reduceRoot = createReduceRoot(new ImageStorage());
+    const state = reduceRoot(undefined, actionToolbarPositionSet(1, 2));
+    expect(selectToolbarPosition(state)).toEqual({x: 1, y: 2});
+})
