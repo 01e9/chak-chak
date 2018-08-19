e0cce72fce955fec3420abc0a9b912c4f95187dc
arteniioleg <oleg@artenii.email>
2018-08-19 14:40:11 +0300

    updated snapshot test of `redux/react/entry/editor.js`

--- src/redux/react/entry/editor.test.js
+++ src/redux/react/entry/editor.test.js
@@ -2,10 +2,8 @@ import React from 'react'
 import createEditor from "@/redux/react/entry/editor";
 import { createMount } from '@material-ui/core/test-utils';
 
-describe('snapshot', () => {
-    it('image', () => {
-        const Editor = createEditor(new Image(100, 100))
-        const wrapper = createMount()(<div><Editor/></div>)
-        expect(wrapper.html()).toMatchSnapshot()
-    })
+it('snapshot', () => {
+    const Editor = createEditor(new Image(100, 100))
+    const wrapper = createMount()(<div><Editor/></div>)
+    expect(wrapper.html()).toMatchSnapshot()
 })
