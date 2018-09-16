ff77fc7e6d907de3f698e63b532c62482ad8c43b
arteniioleg <oleg@artenii.email>
2018-09-16 15:53:31 +0300

    use middlewareAutoDeactivateTool in redux/react/entry/editor

--- src/redux/react/entry/editor.js
+++ src/redux/react/entry/editor.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import ImageStorage from "@/storage/ImageStorage";
 import createReduceRoot from "@/redux/reducers";
 import { ImageStorageProvider } from "@/react/context/ImageStorage";
 import { actionMainImageSet } from "@/redux/actions/mainImage";
 import Editor from "@/redux/react/Editor";
+import middlewareAutoDeactivateTool from "@/redux/middleware/autoDeactivateTool";
 
 export default function createEditor(image) {
     const
         imageStorage = new ImageStorage(),
-        store = createStore(createReduceRoot(imageStorage));
+        store = createStore(createReduceRoot(imageStorage), applyMiddleware(middlewareAutoDeactivateTool));
 
     store.dispatch(actionMainImageSet(image));
 
