9812072d598ccf9f55d7aba38e46661f2e6b4e16
Oleg Artene <hi@01eg.art>
2019-06-03 00:49:09 +0200

    converting to typescript

---
+++ src/utility/canvas.test.ts
@@ -0,0 +1,16 @@
+import { canvasGetAspectRatio, canvasDrawImage } from "@/utility/canvas";
+
+describe('canvasGetAspectRatio', () => {
+    it('valid value', () => {
+        const canvas = document.createElement('canvas');
+        expect(canvasGetAspectRatio(canvas)).toBeGreaterThanOrEqual(1);
+    });
+});
+
+it('canvasDrawImage', () => {
+    const canvas = document.createElement('canvas');
+    expect(canvas).not.toHaveProperty('width');
+    const image = new Image(100, 100);
+    canvasDrawImage(canvas, image);
+    expect(canvas).toHaveProperty('width');
+});
--- src/utility/canvas.ts
+++ src/utility/canvas.ts
@@ -1,24 +1,36 @@
-export function canvasGetAspectRatio(canvas) {
+export function canvasGetAspectRatio(canvas: HTMLCanvasElement): number {
     const
         windowPixelRatio = (window.devicePixelRatio || 1),
         context = canvas.getContext('2d'),
         canvasPixelRatio = (
+            // @ts-ignore
             context.webkitBackingStorePixelRatio ||
+            // @ts-ignore
             context.mozBackingStorePixelRatio ||
+            // @ts-ignore
             context.msBackingStorePixelRatio ||
+            // @ts-ignore
             context.oBackingStorePixelRatio ||
+            // @ts-ignore
             context.backingStorePixelRatio ||
             1
         ),
         aspectRatio = (windowPixelRatio / canvasPixelRatio);
+
     return aspectRatio;
 }
 
-export function canvasDrawImage(canvas, image) {
+export function canvasDrawImage(canvas: HTMLCanvasElement, image: HTMLImageElement) {
     canvas.width = image.naturalWidth || image.width;
     canvas.height = image.naturalHeight || image.height;
     const canvasAspectRatio = canvasGetAspectRatio(canvas);
     canvas.style.width = (canvas.width / canvasAspectRatio) + 'px';
     canvas.style.height = (canvas.height / canvasAspectRatio) + 'px';
-    canvas.getContext('2d').drawImage(image, 0, 0);
+
+    const context = canvas.getContext('2d');
+    if (context) {
+        context.drawImage(image, 0, 0);
+    } else {
+        throw new Error("Cannot get canvas context");
+    }
 }
